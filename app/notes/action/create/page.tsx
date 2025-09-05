e61fc3da13169d06890ce189b451a920d7dbc91e
YURII VORONYCH <voronich97@gmail.com>
2025-09-05 17:07:06 +0300

    Initial commit

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,35 @@
+import NoteForm from "@/components/NoteForm/NoteForm";
+import css from "./CreateNote.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Notehub Create Page",
+  description: "Page that allows you to create new note",
+  openGraph: {
+    title: `Notehub Create Page`,
+    description: "Page that allows you to create new note",
+    url: `https://08-zustand-three-nu.vercel.app/notes/action/create`,
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "og notehub",
+      },
+    ],
+    type: "article",
+  },
+};
+const CreatePage = () => {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+        <NoteForm />
+      </div>
+    </main>
+  );
+};
+
+export default CreatePage;
